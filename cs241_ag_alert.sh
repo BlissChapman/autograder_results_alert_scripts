3e1adc62e80ba3c613f6b7cbb3e86d3f98cc74e0
Jordi Paris Ferrer <jparisferrer@gmail.com>
2017-02-19 22:29:36 -0600

    Made it actually send an @channel, not that fake stuff we had before

--- cs241_ag_alert.sh
+++ cs241_ag_alert.sh
@@ -8,10 +8,11 @@ do
   svn up
   ATIME=`stat -f %Z shell/results.json`
   if [[ "$ATIME" != "$LTIME" ]]
+  #if [ 1 -eq 1 ]
   then
       # send slack notification
       # TODO: Make message specific to AG result and attach random pic of doggo.
-      curl -X POST -H 'Content-type: application/json' --data '{"text":"@channel Shell Autograder results released!"}' $CS241_AG_WEBHOOK
+      curl -X POST -H 'Content-type: application/json' --data '{"text":"<!channel> Shell Autograder results released!"}' $CS241_AG_WEBHOOK
 
       # For local notifications on macOS:
       # osascript -e 'display notification "CS241 AG results were released!" with title "AG Results"'
