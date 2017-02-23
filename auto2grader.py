6b98de8aa348e07966638e2ca8c6ec05ad8d045c
Shreyas Patil <shreyas208@gmail.com>
2017-02-23 01:48:51 -0600

    added hashing

--- auto2grader.py
+++ auto2grader.py
@@ -1,36 +1,55 @@
 import pickledb
 import requests
 import os
+import sys
+import time
 import xml.etree.ElementTree as ET
+import hashlib
+
+DB_HASH_DICT = 'hash_dict'
+SVN_BASE_URL = 'https://subversion.ews.illinois.edu/svn/sp17-cs241/'
 
 def initialize():
     print "Initializing..."
 
-def update_assignment_list(pdb):
-    r = requests.get('https://subversion.ews.illinois.edu/svn/sp17-cs241/srpatil2/', auth=(os.environ['A2G_USERNAME'], os.environ['A2G_PASSWORD']))
+def get_assignment_list():
+    r = requests.get(SVN_BASE_URL + os.environ['A2G_USERNAME'] + '/', auth=(os.environ['A2G_USERNAME'], os.environ['A2G_PASSWORD']))
     htmlstr = r.text
     i = htmlstr.find('<ul>')
     j = htmlstr.find('</ul>') + 5
     if i > -1 and j > -1:
         htmlstr = htmlstr[i:j]
         xmlroot = ET.fromstring(htmlstr)
-        pdb.lrem('assignments')
-        pdb.lcreate('assignments')
+        assignments = []
         for i in range(1, len(xmlroot)):
-            pdb.ladd('assignments', xmlroot[i][0].text[:-1])
+            assignments.append(xmlroot[i][0].text[:-1])
+        return assignments
+    return None
 
+def get_result_hash(assignment):
+    r = requests.get(SVN_BASE_URL + os.environ['A2G_USERNAME'] + '/' + assignment + '/results.json', auth=(os.environ['A2G_USERNAME'], os.environ['A2G_PASSWORD']))
+    if r.status_code != 200:
+        return None
+    return hashlib.md5(r.text).hexdigest()
+
+# MAIN
 db = pickledb.load('db0.db', False)
 
-if (db.get('init')):
-    initialize()
+assignments = get_assignment_list()
+
+if not (db.get('init')):
+    print "First run; building database..." 
+    db.dcreate(DB_HASH_DICT)
+    for assignment in assignments:
+        db.dadd(DB_HASH_DICT, (assignment, get_result_hash(assignment)))
     db.set('init', True)
-    db.lcreate('assignments')
-    db.dcreate('last_updated')
+    db.dump()
+    sys.exit(0)
 
 print 'Updating assignment list...'
-update_assignment_list(db)
+assignments = get_assignment_list()
 print 'Assignments:'
-for i in range(db.llen('assignments')):
-    print '- ' + db.lget('assignments', i)
+for assignment in assignments:
+    print '- ' + assignment + ": " + (db.dget(DB_HASH_DICT, assignment))
 
 db.dump()
